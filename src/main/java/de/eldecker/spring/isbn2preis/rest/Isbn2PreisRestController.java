194b526cf5b5b50b298064a2299786e3fdf495d1
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-01-16 16:10:13 +0100

    Isbn2PreisRestController: Imports geloescht

--- src/main/java/de/eldecker/spring/isbn2preis/rest/Isbn2PreisRestController.java
+++ src/main/java/de/eldecker/spring/isbn2preis/rest/Isbn2PreisRestController.java
@@ -1,16 +1,13 @@
 package de.eldecker.spring.isbn2preis.rest;
 
-import static java.lang.Double.NEGATIVE_INFINITY;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
